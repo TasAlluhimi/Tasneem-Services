338a60516e04dc1d26ad4186d857fa6fef282bd9
tasalluhimi <tasneem.luhimi@gmail.com>
2024-01-01 00:34:30 +0300

    some edits

--- JS/Main.js
+++ JS/Main.js
@@ -7,3 +7,22 @@ console.log('test');
           navToggle.item(i).classList.toggle("hidden");
         }
       };
+
+      document.addEventListener('DOMContentLoaded', function () {
+        const links = document.querySelectorAll('a[href^="#"]');
+        links.forEach(link => {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+    });
