4c84cbf7a5fddcbe78f5187c68b889f4026cd574
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-31 23:51:40 +0300

    first commit

---
+++ JS/Main.js
@@ -0,0 +1,9 @@
+console.log('test');
+
+    // for burgur menu 
+    document.getElementById("hamburger").onclick = function toggleMenu() {
+        const navToggle = document.getElementsByClassName("toggle");
+        for (let i = 0; i < navToggle.length; i++) {
+          navToggle.item(i).classList.toggle("hidden");
+        }
+      };
